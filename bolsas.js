1b85a6f4c857c353cfbeee85844ca126bd903704
heloisa <125582181+loisaaz@users.noreply.github.com>
2024-03-08 12:24:18 -0300

    update bolsas

--- bolsas.js
+++ bolsas.js
@@ -1,5 +1,5 @@
 let slideIndex = [1,1];
-let slideId = ["mySlides2"]
+let slideId = ["mySlides1", "mySlides2"]
 showSlides(1, 0);
 showSlides(1, 1);
 
@@ -17,4 +17,4 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";  
 }
-
+ 
