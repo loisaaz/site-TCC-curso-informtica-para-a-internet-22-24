ca1dffef97f290b121306a06f9be4d845b5eb005
heloisa <125582181+loisaaz@users.noreply.github.com>
2024-04-25 12:34:00 -0300

    adicionado menu

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*inicio menu */
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
-
-function toggleSubmenu(event) {
-  event.preventDefault();
-  var submenu = document.querySelector(".submenu");
-  submenu.style.display = (submenu.style.display === "block") ? "none" : "block";
-}
-/*final menu */
 
 /* DOM (cliques) */
 let count = 0;
