baca67f035e02efbf9504719a86ffa57db3289b0
heloisa <125582181+loisaaz@users.noreply.github.com>
2024-05-24 12:07:46 -0300

    adicionado arquivo teste.js e javascript responsivo

---
+++ teste.js
@@ -0,0 +1,6 @@
+//Javascript of responsive navigation menu
+const menuBtn = document.querySelector(".mneu-btn")
+
+menuBtn.addEventListener("click", ()=> {
+    menuBtn.classList.toggle("active");
+});
